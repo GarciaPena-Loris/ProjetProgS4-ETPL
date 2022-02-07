d574cca6064ff07f95c9978993ef357e630564a4
Garcia-Pena Loris <loris.garcia-pena@etu.umontpellier.fr>
2022-02-07 14:53:42 +0100

    creation des fonctions de bases

--- progammes/CestQuiGame/src/App.java
+++ progammes/CestQuiGame/src/App.java
@@ -1,12 +1,34 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+import java.io.*;
+
 public final class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
         // Creer et afficher le menu
         // en fonction des choix créer une interface de QuiEstCe
         // (difficulte / sauvegarde)
-        //creer une "Game" avec les différents parametres et le lien du JSON
-        Game partieEnCour = new Game(Difficulte.normal, "c:/documents/listePersonnages.json");
+        // creer une "Game" avec les différents parametres et le lien du JSON
+
+        // converti le JSON en String[]
+
+        // parsing file "JSONExample.json"
+        JSONObject js = (JSONObject) new JSONParser().parse(new FileReader("Test.json"));
+
+        String cheminVersImages = (String) js.get("images");
+        String ligne = (String) js.get("ligne");
+        String colonne = (String) js.get("colonne");
+        JSONObject personnages = (JSONObject) js.get("personnages");
+
+        System.out.println("cheminVersImages : " + cheminVersImages);
+        System.out.println("ligne : " + ligne);
+        System.out.println("colonne : " + colonne);
+        System.out.println(personnages);
 
         
+        //Game partieEnCour = new Game(Difficulte.normal, personnages, ligne, colonne);
+        //partieEnCour.verifierElimination();
+
     }
 }
--- progammes/CestQuiGame/src/Game.java
+++ progammes/CestQuiGame/src/Game.java
@@ -9,61 +9,34 @@ import org.json.simple.parser.*;
 
 public class Game {
     private Difficulte difficulte;
-    private String[] personnageChoisi;
-    // private HashMap<String, Boolean> listePersonnages;
-    private ArrayList<ArrayList<HashMap<ArrayList<HashMap<String, String>>, Boolean>>> listePersonnages;
+    private JSONObject personnageChoisi;
+    private ArrayList<ArrayList<HashMap<JSONObject, Boolean>>> listePersonnages;
 
-    public Game(Difficulte d, String cheminJSON) throws Exception {
+    public Game(Difficulte d, JSONObject JSONPersonnages) throws Exception {
         this.difficulte = d;
         // creationListePersonnages
-        HashMap<String, ?> contenueJSON = recupererJson(cheminJSON);
-        int ligne = (int) contenueJSON.get("ligne");
-        int colonne = (int) contenueJSON.get("colonne");
-        for (int i = 0; i < ligne; i++) {
-            for (int j = 0; j < colonne; j++) {
-                // for ( ArrayList listePersonnage : contenueJSON.get("possibilites")) {
+        
 
-                // }
-            }
-        }
 
-        int rand = 0 + (int) (Math.random() * ((contenueJSON.get("possibilites").toString().length() - 0) + 1));
+        //int rand = 0 + (int) (Math.random() * ((contenueJSON.get("possibilites").toString().length() - 0) + 1));
     }
 
-    public Game(Difficulte d, String cheminPersonnages, String cheminSauvegarde) {
+    public Game(Difficulte d, JSONObject JSONPersonnages, String cheminSauvegarde) {
 
     }
 
-    public HashMap<String, ?> recupererJson(String cheminJSON) throws Exception {
-        // recuperer le JSON
-        // converti le JSON en String[]
-        // {
-        // "images": "personnages/",
-        // "ligne": "3",
-        // "colonne": "8",
-        // "possibilites": {
-        // "0": { "fichier": "samuel.jpg", "prenom": "Samuel", "genre": "homme",
-        // "cheveux": "blanc", "lunettes": "oui", "chauve": "oui", ... },
-        // "1": { "fichier": "leon.jpg", "prenom": "Léon", "genre": "homme", "cheveux":
-        // "blond", "lunettes": "oui", "chauve": "non", ... },
-        // "2": { "fichier": "simon.jpg","prenom": "Simon", "genre": "homme", "cheveux":
-        // "blanc", "lunettes": "oui", "chauve": "non", ...},
-        // ...
-        // }
-        // }
+    public boolean verifierReponse(String question) {
 
-        // TEST JSON
-        // parsing file "JSONExample.json"
-        Object ob = new JSONParser().parse(new FileReader("Test.json"));
-
-        // typecasting ob to JSONObject
-        JSONObject js = (JSONObject) ob;
-
-        String firstName = (String) js.get("firstName");
-        String lastName = (String) js.get("lastName");
-
-        System.out.println("First name is: " + firstName);
-        System.out.println("Last name is: " + lastName);
-        return null;
+        return false;
     }
+
+    public void verifierElimination() {
+        //verifier qu'il a pas eliminer la mauvaise personne
+
+        //sauvegarder
+        //double boucle dans laquelle tu ajoute chaque element de "listePersonnages" dans notre Json de sauvegarde.
+        //enregistrer quelquepart
+
+    }
+    
 }
