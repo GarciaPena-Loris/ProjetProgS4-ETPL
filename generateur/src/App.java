83220bd9b2259a69417e4353a1d97f9649c3b0ef
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2022-03-18 16:20:01 +0100

    creation partie générateur json

---
+++ generateur/src/App.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+
+//javafx
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("pageGenerateur.fxml"));
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Générateur Json pour -Qui est ce ?-");
+            File logo = new File("images/iconeGenerateur.png");
+            primaryStage.getIcons().add(new Image("file:///" + logo.getAbsolutePath()));
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+}
---
+++ generateur/src/pageGenerateurController.java
@@ -0,0 +1,83 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class pageGenerateurController {
+
+    private String cheminVersImage;
+    private String ligne;
+    private String colonne;
+    private ArrayList<String> listePersonnages;
+
+    @FXML
+    private AnchorPane MainAnchorPane;
+
+    @FXML
+    private AnchorPane bottomAnchorPane;
+
+    @FXML
+    private Text colonnesText;
+
+    @FXML
+    private Text explicationText;
+
+    @FXML
+    private Text ligneText;
+
+    @FXML
+    private AnchorPane middleAnchorPane;
+
+    @FXML
+    private ScrollBar scrollBar;
+
+    @FXML
+    private TextField textField1;
+
+    @FXML
+    private TextField textField2;
+
+    @FXML
+    private AnchorPane topAnchorPane;
+
+    @FXML
+    private Button choixImagesButton;
+
+    @FXML
+    private Button validerButton;
+    
+
+    @FXML
+    protected void initialize() {
+        File logoFile = new File("images/logoGenerateur.png");
+        Image logoImage = new Image("file:///" + logoFile.getAbsolutePath());
+        ImageView logoVimage = new ImageView(logoImage);
+        logoVimage.setFitHeight(137.);
+        logoVimage.setFitWidth(904.);
+        topAnchorPane.getChildren().add(logoVimage);
+    }
+    
+    @FXML
+    void choixImageEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void textFieldEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void validerButtonEvent(ActionEvent event) {
+
+    }
+
+}
